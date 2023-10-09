351d304beb6b7611bdb85d590e133cabe0ea7fad
cono200 <tatattian04@gmail.com>
2023-10-09 09:40:57 -0700

    Agregar archivos de proyecto.

---
+++ MVVM_implementacion_SLE/VistaModelo/VMpatron.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_implementacion_SLE.VistaModelo
+{
+    class VMpatron : BaseViewModel
+    {
+        #region VARIABLES
+        string _Texto;
+        #endregion
+        #region CONTRUCTOR
+
+        #endregion
+        #region OBJETOS
+
+        public string Texto
+        {
+            get { return _Texto; }
+            set { SetValue(ref _Texto, value); }
+        }
+
+        #endregion
+        #region PROCESOS
+
+        public async Task ProcesoAsyncrono()
+        {
+
+        }
+
+        public void ProcesoSimple()
+        {
+
+        }
+
+        #endregion
+
+
+
+    }
+
+}
